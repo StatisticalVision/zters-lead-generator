ef329cbe4a2a58ad6fb52f8386c4417b98ed3d2f
Saina <saina.sheini@hahn.agency>
2023-08-18 12:37:55 -0500

    Created package file 2

---
+++ install_packages.r
@@ -0,0 +1,11 @@
+# install packages
+install.packages("data.table")
+install.packages("stringi")
+install.packages("DBI")
+install.packages("RPostgres")
+install.packages("dotenv")
+install.packages("slackr")
+install.packages("rjson")
+install.packages("httr")
+install.packages("zoo")
+install.packages("tidyverse")
